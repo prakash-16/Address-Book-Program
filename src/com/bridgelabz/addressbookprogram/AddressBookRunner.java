b460739183358cab4e23e20a0cfd09df471f7cec
Prakash Goud <prakash.goud@morningstar.com>
2021-09-03 00:17:46 +0530

    [Prakash] - Added the feature to check duplicate entry in address book

---
+++ src/com/bridgelabz/addressbookprogram/AddressBookRunner.java
@@ -0,0 +1,45 @@
+package com.bridgelabz.addressbookprogram;
+
+import java.util.*;
+
+public class AddressBookRunner {
+
+	public static void main(String[] args) {
+		System.out.println(" Welcome to Address Book Program ");
+		AddressBookProgram addressBookOne = new AddressBookProgram();
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			System.out.println("Enter your first name :- ");
+			String firstName = sc.nextLine();
+			System.out.println("Enter your last name :- ");
+			String lastName = sc.nextLine();
+			System.out.println("Enter your address :- ");
+			String address = sc.nextLine();
+			System.out.println("Enter your city :- ");
+			String city = sc.nextLine();
+			System.out.println("Enter your state :- ");
+			String state = sc.nextLine();
+			System.out.println("Enter your zip code :- ");
+			String zip = sc.nextLine();
+			System.out.println("Enter your contact number :- ");
+			String contactNumber = sc.nextLine();
+			System.out.println("Enter your email :- ");
+			String email = sc.nextLine();
+			boolean result = addressBookOne.addContactDetails(firstName, lastName, address, city, state, zip, contactNumber,
+					email);
+			if(result == true) {
+				System.out.println("Contact added successfully");
+			}
+			else {
+				System.out.println("Please enter valid input");
+			}
+			System.out.println("Do you wish to add more contacts (Yes/No)");
+			String choice = sc.nextLine();
+			if(choice.toLowerCase().equals("no")) {
+				break;
+			}
+		}
+		addressBookOne.showDetails();
+	}
+
+}
